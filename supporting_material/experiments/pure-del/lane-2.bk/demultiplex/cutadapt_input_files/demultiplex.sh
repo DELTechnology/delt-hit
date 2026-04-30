7aeac069aec010f5791a9d90eb022af16ce45748
adrianomartinelli <adrianom@student.ethz.ch>
2026-04-30 18:46:08 -0300

    add pure del; run favalli across lines

---
+++ supporting_material/experiments/pure-del/lane-2.bk/demultiplex/cutadapt_input_files/demultiplex.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+# make sure you installed pigz with `brew install pigz` to enable parallel processing
+
+mkdir "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files"
+ln -sf "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/328321_2-230906_MK_TSD504704_S2_R1_001_UP_fl2.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/0-S0.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/0-S0.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/0-S0.cutadapt.log"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/1-C0.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/1-C0.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/1-C0.cutadapt.log"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/2-B0.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/2-B0.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/2-B0.cutadapt.log"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/3-C1.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/3-C1.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/3-C1.cutadapt.log"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/4-B1.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/4-B1.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/4-B1.cutadapt.log"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/5-C2.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/5-C2.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/5-C2.cutadapt.log"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/6-B2.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/6-B2.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/6-B2.cutadapt.log"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/7-C3.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/7-C3.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/7-C3.cutadapt.log"
+
+mv "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz"
+
+cutadapt "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/input.fastq.gz" \
+-o "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" \
+-e 0.0 --no-indels \
+-g "^file:/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_input_files/8-S1.fastq" \
+--rename '{id} {comment}?{adapter_name}' \
+--discard-untrimmed \
+--json="/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/8-S1.cutadapt.json" \
+--cores=10 2>&1 | tee "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/8-S1.cutadapt.log"
+
+gzip -cd "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/out.fastq.gz" | awk 'NR % 4 == 1' | gzip -c > "/Users/adrianomartinelli/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/demultiplex/cutadapt_output_files/reads_with_adapters.gz" || exit
