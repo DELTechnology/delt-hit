44f9cf6c7931f05ddc8f47702f22672e6724a5e5
adrianomartinelli <adrianom@student.ethz.ch>
2026-05-02 22:54:18 +0200

    enrichment scripts

--- supporting_material/experiments/pure-del/lane-1/analysis/his_pure_sp/counts/enrichment_counts.R
+++ supporting_material/experiments/pure-del/lane-1/analysis/his_pure_sp/counts/enrichment_counts.R
@@ -5,9 +5,9 @@ suppressPackageStartupMessages({
 })
 
 args <- list(
-  data_path = "/work/FAC/FBM/DBC/mrapsoma/prometex/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/analysis/his_pure_up/data.csv",
-  samples_path = "/work/FAC/FBM/DBC/mrapsoma/prometex/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/analysis/his_pure_up/samples.csv",
-  save_dir = "/work/FAC/FBM/DBC/mrapsoma/prometex/projects/delt-hit/supporting_material/experiments/pure-del/lane-2/analysis/his_pure_up/counts",
+  data_path = "/work/FAC/FBM/DBC/mrapsoma/prometex/projects/delt-hit/supporting_material/experiments/pure-del/lane-1/analysis/his_pure_sp/data.csv",
+  samples_path = "/work/FAC/FBM/DBC/mrapsoma/prometex/projects/delt-hit/supporting_material/experiments/pure-del/lane-1/analysis/his_pure_sp/samples.csv",
+  save_dir = "/work/FAC/FBM/DBC/mrapsoma/prometex/projects/delt-hit/supporting_material/experiments/pure-del/lane-1/analysis/his_pure_sp/counts",
   cpm      = FALSE
 )
 
